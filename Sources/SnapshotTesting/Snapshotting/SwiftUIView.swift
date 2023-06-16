a463d0aee052ca0e7add4a7bb2e98cc81e491d07
Cihat Gündüz <commits@cihatguenduez.de>
2023-06-16 14:23:03 +0200

    Add initialization support for SwiftUI layout from PreviewDevice

--- Sources/SnapshotTesting/Snapshotting/SwiftUIView.swift
+++ Sources/SnapshotTesting/Snapshotting/SwiftUIView.swift
@@ -1,6 +1,7 @@
 #if canImport(SwiftUI)
 import Foundation
 import SwiftUI
+import XCTest
 
 /// The size constraint for a snapshot (similar to `PreviewLayout`).
 public enum SwiftUISnapshotLayout {
@@ -12,6 +13,61 @@ public enum SwiftUISnapshotLayout {
   case fixed(width: CGFloat, height: CGFloat)
   /// Fit the view to the ideal size that fits its content.
   case sizeThatFits
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 13.0, tvOS 13.0, *)
+  public static func previewDevice(_ previewDevice: PreviewDevice, orientation: ViewImageConfig.Orientation = .portrait) -> Self {
+    switch previewDevice.rawValue {
+    case "iPhone 8":
+      return .device(config: .iPhone8(orientation))
+    case "iPhone 8 Plus":
+      return .device(config: .iPhone8Plus(orientation))
+    case "iPhone X":
+      return .device(config: .iPhoneX(orientation))
+    case "iPhone Xs":
+      return .device(config: .iPhoneX(orientation))
+    case "iPhone Xs Max", "iPhone 11 Pro Max":
+      return .device(config: .iPhoneXsMax(orientation))
+    case "iPhone Xʀ", "iPhone 11", "iPhone 11 Pro":
+      return .device(config: .iPhoneXr(orientation))
+    case "iPhone SE (2nd generation)", "iPhone SE (3rd generation)":
+      return .device(config: .iPhoneSe(orientation))
+    case "iPhone 12 mini", "iPhone 13 mini":
+      return .device(config: .iPhone13Mini(orientation))
+    case "iPhone 12":
+      return .device(config: .iPhone12(orientation))
+    case "iPhone 12 Pro":
+      return .device(config: .iPhone12Pro(orientation))
+    case "iPhone 12 Pro Max":
+      return .device(config: .iPhone12ProMax(orientation))
+    case "iPhone 13 Pro":
+      return .device(config: .iPhone13Pro(orientation))
+    case "iPhone 13 Pro Max", "iPhone 14 Plus", "iPhone 14 Pro Max":
+      return .device(config: .iPhone13ProMax(orientation))
+    case "iPhone 13", "iPhone 14", "iPhone 14 Pro":
+      return .device(config: .iPhone13(orientation))
+    case "Apple TV":
+      return .fixed(width: 1920, height: 1080)
+    default:
+      if previewDevice.rawValue.contains("Apple TV 4K") {
+        return .fixed(width: 3840, height: 2160)
+      } else if previewDevice.rawValue.contains("iPad") {
+        if previewDevice.rawValue.contains("(11-inch)") {
+          return .device(config: .iPadPro11(orientation))
+        } else if previewDevice.rawValue.contains("(12.9-inch)") {
+          return .device(config: .iPadPro12_9(orientation))
+        } else if previewDevice.rawValue.contains("iPad mini") {
+          return .device(config: .iPadMini(orientation))
+        } else {
+          return .device(config: .iPad9_7(orientation))
+        }
+      }
+
+      XCTFail("Unsupported device: \(previewDevice.rawValue)")
+      return .sizeThatFits
+    }
+  }
+  #endif
 }
 
 #if os(iOS) || os(tvOS)
